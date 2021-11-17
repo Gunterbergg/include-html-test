c802af4115a1f394aa377e2662469b161651bb9c
GustavoRossi <gustavo.rocha@lumetec.com.br>
2021-11-17 17:27:54 -0300

    Innitial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function onTodoLoad(){
+	let list = document.querySelector('.list')
+	console.log("list:" + list);
+	const CHECK = {status:'completed', icon:'bx bxs-check-circle'};
+	const UNCHECK = {status:'not-completed', icon:'bx bx-circle'}
+	let id = 0;
+	let itemsList = []
+}
